4e210e8aae43c1035d8ab757f842762a3770e4cb
brightestsirius <katherina.shypovskaya@gmail.com>
2024-01-11 18:11:04 +0200

    lesson_04 – intro

--- lesson_04/src/components/Todos/TodosColorPicker.jsx
+++ lesson_04/src/components/Todos/TodosColorPicker.jsx
@@ -2,8 +2,6 @@ import React, {memo} from "react";
 import { MuiColorInput } from "mui-color-input";
 
 export default memo(function TodosColorPicker({ setColor, color }) {
-  console.log(`in TodosColorPicker`);
-
   const handleColor = (value) => setColor(value);
   return <MuiColorInput value={color} onChange={handleColor} />;
 })
