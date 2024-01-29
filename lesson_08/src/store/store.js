3cd3114f4be5a613c71f35eedea4d0acb6cdf36c
brightestsirius <katherina.shypovskaya@gmail.com>
2024-01-29 18:13:54 +0200

    lesson_08 – init

--- lesson_08/src/store/store.js
+++ lesson_08/src/store/store.js
@@ -3,6 +3,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import users from "./users/usersSlice";
 import filter from "./filter/reducer";
 
+import logger from 'redux-logger'
+
 export default configureStore({
   reducer: { users, filter },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
